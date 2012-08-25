b3d6ca76f7fca229f87d66ae553135bcc105e88c
ultimate <ultimatej@gmx.net>
2012-08-25 11:36:51 +0200

    corrected EnumFinder-Utility

--- src/test/java/ultimate/karoapi4j/EnumFinder.java
+++ src/test/java/ultimate/karoapi4j/EnumFinder.java
@@ -5,22 +5,24 @@ import java.net.URL;
 
 import ultimate.karoapi4j.utils.sync.Refreshable;
 import ultimate.karoapi4j.utils.web.URLLoader;
-import ultimate.karoapi4j.utils.web.URLLoaderThread;
+import ultimate.karoapi4j.utils.web.urlloaders.StringURLLoaderThread;
 
 public class EnumFinder implements Refreshable<String>
 {
 	public static final String	url			= "http://reloaded.karopapier.de/api/games";
 	public static final String	params		= "limit=100";
-	public static final String	enumName	= "status";
+	public static final String	enumName	= "tcrash";
+	
+	private static URLLoader<String> loader;
 
 	public static void main(String[] args) throws MalformedURLException
 	{
 //		for(int i = 40000; i < 40100; i++)
 //		{
 //			URLLoader loader = new URLLoaderThread(new URL("http://reloaded.karopapier.de/api/games/" + i + "/info"), "");
-			URLLoader loader = new URLLoaderThread(new URL(url), "");
+			loader = new StringURLLoaderThread(new URL(url));
 			System.out.println("loading...");
-			loader.loadURL(new EnumFinder());
+			loader.load(new EnumFinder());
 //		}
 	}
 
@@ -28,11 +30,15 @@ public class EnumFinder implements Refreshable<String>
 	public void onRefresh(String refreshed)
 	{
 		System.out.println("load finished, analyzing...");
+		System.out.println(loader.getLoadedContent());
 		int index = refreshed.indexOf(enumName, 0);
+		int i = 0;
 		while(index >= 0)
 		{
-			System.out.println(refreshed.substring(index, refreshed.indexOf(",", index)));
+			System.out.println(i++ + ": " + refreshed.substring(index, refreshed.indexOf(",", index)));
 			index = refreshed.indexOf(enumName, index + 1);
 		}
+		System.out.println("analyzation done");
+		System.exit(0);
 	}
 }
