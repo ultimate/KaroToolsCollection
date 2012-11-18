26760c8553b37112bc7e8579d746c8e6180caa40
ultimate <ultimatej@gmx.net>
2012-11-18 18:35:17 +0100

    added enum-values

--- src/main/java/ultimate/karoapi4j/enums/EnumTC.java
+++ src/main/java/ultimate/karoapi4j/enums/EnumTC.java
@@ -5,4 +5,26 @@ public enum EnumTC
 	allowed,
 	forbidden,
 	free,
+	random;
+	
+	public int getValue()
+	{
+		if(this.equals(allowed))
+			return 1;
+		if(this.equals(forbidden))
+			return 2;
+		if(this.equals(free))
+			return 0;
+		return -1;
+	}
+	
+	public static EnumTC getByValue(int value)
+	{
+		for(EnumTC e : values())
+		{
+			if(e.getValue() == value)
+				return e;
+		}
+		return null;
+	}
 }
