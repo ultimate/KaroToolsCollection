195bc4ddf921e1fb05fc1f969fd3ff4f2b058412
ultimate <ultimatej@gmx.net>
2016-11-07 17:31:37 +0100

    test

--- Tools/src/ultimate/karopapier/test/TestSomething.java
+++ Tools/src/ultimate/karopapier/test/TestSomething.java
@@ -1,6 +1,8 @@
 package ultimate.karopapier.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import ultimate.karoapi4j.utils.URLLoaderUtil;
@@ -18,5 +20,16 @@ public class TestSomething
 		String params = URLLoaderUtil.formatParameters(parameters);
 		
 		System.out.println(url + "?" + params);
+		
+		
+		HashMap<Integer, String> map = new HashMap<Integer, String>();		
+		map.put(2, "2");
+		map.put(3, "3");
+		map.put(5, "5");
+		map.put(7, "7");
+		map.put(6, "6");
+		map.put(1, "1");
+		map.put(4, "4");		
+		System.out.println(map.values());
 	}
 }
