36ab5563dcfa5f921697795f0c3b7b2d73508dd8
ultimate <ultimatej@gmx.net>
2016-11-21 12:07:07 +0100

    Wiki-Login umgesetzt

--- src/main/java/ultimate/karoapi4j/KaroWikiURLs.java
+++ src/main/java/ultimate/karoapi4j/KaroWikiURLs.java
@@ -5,12 +5,16 @@ public class KaroWikiURLs
 	/**
 	 * http://wiki.karopapier.de/api.php<br>
 	 */
-	public static final String	API_BASE					= "http://wiki.karopapier.de/api.php";
+	public static final String	API_BASE						= "http://wiki.karopapier.de/api.php";
 
 	public static final String	PARAMETER_ACTION				= "action";
+	public static final String	PARAMETER_FORMAT				= "format";
 
-	public static final String	ACTION_LOGIN				= "login";
+	public static final String	ACTION_LOGIN					= "login";
 	public static final String	PARAMETER_ACTION_LOGIN_USER		= "lgname";
 	public static final String	PARAMETER_ACTION_LOGIN_PASSWORD	= "lgpassword";
+	public static final String	PARAMETER_ACTION_LOGIN_TOKEN	= "lgtoken";
+
+	public static final String	FORMAT_JSON						= "json";
 
 }
--- src/test/java/ultimate/karoapi4j/wiki/KaroWikiLoaderTest.java
+++ src/test/java/ultimate/karoapi4j/wiki/KaroWikiLoaderTest.java
@@ -1,23 +1,37 @@
 package ultimate.karoapi4j.wiki;
 
-import java.net.CookieHandler;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 import junit.framework.TestCase;
+import ultimate.karoapi4j.utils.PropertiesUtil;
 
 public class KaroWikiLoaderTest extends TestCase
 {
-	public void testLogin() throws Exception
-	{
-		
+	private static String username;
+	private static String password;
+	
+	static {
+		try
+		{
+			Properties p = PropertiesUtil.loadProperties(new File("src/test/resources/wiki.properties"));
+			username = p.getProperty("username");
+			password = p.getProperty("password");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
-	
-	
-	
-	
+	public void testLogin() throws Exception
+	{
+		KaroWikiLoader wl = new KaroWikiLoader();
+		assertTrue(wl.login(username, password));
+	}	
 	
 	public void testSomething() throws Exception
 	{
-		System.out.println(CookieHandler.getDefault());
 	}
 }
