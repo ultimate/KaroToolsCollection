46512d74c49f6fa0fb3edccdaa03975b972b2e69
ultimate <ultimatej@gmx.net>
2019-08-23 21:11:42 +0200

    GID-Fix

--- Tools/src/ultimate/karopapier/test/GameSeriesEditorForCCC4.java
+++ Tools/src/ultimate/karopapier/test/GameSeriesEditorForCCC4.java
@@ -11,6 +11,7 @@ import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -20,6 +21,7 @@ import muskel2.model.Game;
 import muskel2.model.GameSeries;
 import muskel2.model.Rules;
 import muskel2.model.series.BalancedGameSeries;
+import ultimate.karoapi4j.utils.PropertiesUtil;
 
 public class GameSeriesEditorForCCC4
 {
@@ -55,50 +57,74 @@ public class GameSeriesEditorForCCC4
 		if(result != JOptionPane.OK_OPTION)
 			return;
 		
-		Change[] changes = new Change[] {
-			new Change(6, 0),
-			new Change(7, 1),
-			new Change(10, 2),
-			new Change(11, 1),
-			new Change(12, 1),
-			new Change(16, 0),
-			new Change(18, 0),
-			new Change(20, 1),
-		};
-		
-		Field f = Rules.class.getDeclaredField("zzz");
-		f.setAccessible(true);
-		
+		int option = 2;
+
 		boolean changed = false;
-		for(Change change: changes)
+		if(option == 1)
 		{
-			Rules r = gs.getRules(change.challenge-1);
-			System.out.println("Challenge #" + change.challenge);
-			System.out.println("current ZZZ = null ( " + r.getMinZzz() + " - " + r.getMaxZzz() + " )");
-			System.out.println("Games: ");
 			
-			List<Game> gamesToChange = new ArrayList<Game>();
-			for(Game g: gs.getGames())
+			Change[] changes = new Change[] {
+				new Change(6, 0),
+				new Change(7, 1),
+				new Change(10, 2),
+				new Change(11, 1),
+				new Change(12, 1),
+				new Change(16, 0),
+				new Change(18, 0),
+				new Change(20, 1),
+			};
+			
+			Field f = Rules.class.getDeclaredField("zzz");
+			f.setAccessible(true);
+			
+			for(Change change: changes)
 			{
-				if(g.getName().contains("Challenge " + change.challenge + ".") && !g.isCreated())
+				Rules r = gs.getRules(change.challenge-1);
+				System.out.println("Challenge #" + change.challenge);
+				System.out.println("current ZZZ = null ( " + r.getMinZzz() + " - " + r.getMaxZzz() + " )");
+				System.out.println("Games: ");
+				
+				List<Game> gamesToChange = new ArrayList<Game>();
+				for(Game g: gs.getGames())
 				{
-					gamesToChange.add(g);
-					System.out.println("- " + g.getName() + " @@@ ZZZ = " + g.getRules().getZzz() + " ( " + g.getRules().getMinZzz() + " - " + g.getRules().getMaxZzz() + " )");
+					if(g.getName().contains("Challenge " + change.challenge + ".") && !g.isCreated())
+					{
+						gamesToChange.add(g);
+						System.out.println("- " + g.getName() + " @@@ ZZZ = " + g.getRules().getZzz() + " ( " + g.getRules().getMinZzz() + " - " + g.getRules().getMaxZzz() + " )");
+					}
+				}
+	
+				result = JOptionPane.showConfirmDialog(null, "ZZZ für angezeigte Spiele ändern? " + r.getMinZzz() + " ===> " + change.zzz);
+				if(result == JOptionPane.OK_OPTION)
+				{
+					for(Game g: gamesToChange)
+					{
+						g.getRules().setZzz(change.zzz);
+						f.set(g.getRules(), change.zzz);
+						g.setName(g.getName().replace("ZZZ=" + r.getMinZzz(), "ZZZ=" + change.zzz));
+						System.out.println("- " + g.getName() + " @@@ ZZZ = " + g.getRules().getZzz() + " ( " + g.getRules().getMinZzz() + " - " + g.getRules().getMaxZzz() + " )");
+					}
+					r.setZzz(change.zzz);
+					changed = true;
 				}
 			}
-
-			result = JOptionPane.showConfirmDialog(null, "ZZZ für angezeigte Spiele ändern? " + r.getMinZzz() + " ===> " + change.zzz);
-			if(result == JOptionPane.OK_OPTION)
+		}
+		else if(option == 2)
+		{
+			Properties gids = PropertiesUtil.loadProperties(new File(file.getParentFile().getAbsolutePath() + "/czzzcc4-gid.properties.63"));
+			
+			String cid;
+			int gid;
+			for(Game g: gs.getGames())
 			{
-				for(Game g: gamesToChange)
+				if(g.getName().contains("Challenge 10."))
 				{
-					g.getRules().setZzz(change.zzz);
-					f.set(g.getRules(), change.zzz);
-					g.setName(g.getName().replace("ZZZ=" + r.getMinZzz(), "ZZZ=" + change.zzz));
-					System.out.println("- " + g.getName() + " @@@ ZZZ = " + g.getRules().getZzz() + " ( " + g.getRules().getMinZzz() + " - " + g.getRules().getMaxZzz() + " )");
+					cid = g.getName().substring("CraZZZy Crash Challenge 4 - Challenge ".length(), "CraZZZy Crash Challenge 4 - Challenge ".length()+5).trim();
+					gid = Integer.parseInt(gids.getProperty(cid));
+					System.out.println(cid + " : " + g.getId() + " => " + gid);
+					g.setId(gid);
+					changed = true;
 				}
-				r.setZzz(change.zzz);
-				changed = true;
 			}
 		}
 
