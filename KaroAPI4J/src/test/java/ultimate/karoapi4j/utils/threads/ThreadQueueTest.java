138cd76096f72918c22f9f64e60fe6252f472426
ultimate <ultimatej@gmx.net>
2022-01-20 14:51:53 +0100

    added (yet empty) tests

---
+++ KaroAPI4J/src/test/java/ultimate/karoapi4j/utils/threads/ThreadQueueTest.java
@@ -0,0 +1,15 @@
+package ultimate.karoapi4j.utils.threads;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadQueueTest
+{
+	@Test
+	public void test_something()
+	{
+		fail("not implemented");
+	}
+
+}
---
+++ KaroAPI4J/src/test/java/ultimate/karoapi4j/utils/web/URLLoaderTest.java
@@ -0,0 +1,15 @@
+package ultimate.karoapi4j.utils.web;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class URLLoaderTest
+{
+	@Test
+	public void test_something()
+	{
+		fail("not implemented");
+	}
+
+}
