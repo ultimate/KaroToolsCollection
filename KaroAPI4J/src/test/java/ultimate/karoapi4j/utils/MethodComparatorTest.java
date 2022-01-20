a81d3ab7a30ce96b930ea810b225972cfc62d0a6
ultimate <ultimatej@gmx.net>
2022-01-20 14:51:53 +0100

    added (yet empty) tests

---
+++ KaroAPI4J/src/test/java/ultimate/karoapi4j/utils/MethodComparatorTest.java
@@ -0,0 +1,14 @@
+package ultimate.karoapi4j.utils;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class MethodComparatorTest
+{
+	@Test
+	public void test_something()
+	{
+		fail("not implemented");
+	}
+}
