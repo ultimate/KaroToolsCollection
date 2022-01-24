a9f16630137748c92ac5b0cafe8eeb93e0a2c13e
ultimate <ultimatej@gmx.net>
2022-01-24 16:49:03 +0100

    added PropertiesUtilTest

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/utils/PropertiesUtil.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/utils/PropertiesUtil.java
@@ -46,29 +46,4 @@ public class PropertiesUtil
 		os.flush();
 		os.close();
 	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		// TODO move to test
-		Properties p = new Properties();
-		p.setProperty("lala", "1");
-		p.setProperty("xxxx", "yyyy");
-		for(int i = 0; i < 100; i++)
-			p.setProperty("x." + i, i + "_x");
-		
-		File f1 = new File(".t1");
-		File f2 = new File(".t2");
-		
-		storeProperties(f1, p, "some comments", false);
-		storeProperties(f2, p, "some comments", true);
-		
-		Properties p1 = loadProperties(f1, false);
-		Properties p2 = loadProperties(f2, true);
-		
-		System.out.println(p1.getProperty("lala"));
-		System.out.println(p1.getProperty("xxxx"));
-		
-		System.out.println(p2.getProperty("lala"));
-		System.out.println(p2.getProperty("xxxx"));
-	}
 }
--- KaroAPI4J/src/test/java/ultimate/karoapi4j/utils/PropertiesUtilTest.java
+++ KaroAPI4J/src/test/java/ultimate/karoapi4j/utils/PropertiesUtilTest.java
@@ -1,14 +1,54 @@
 package ultimate.karoapi4j.utils;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
 
 public class PropertiesUtilTest
 {
 	@Test
-	public void test_something()
+	public void test_loadProperties() throws IOException
 	{
-		fail("not implemented");
+		File file = new File("src/test/resources/test.properties");
+		Properties p = PropertiesUtil.loadProperties(file);
+		assertEquals("testvalue", p.getProperty("testkey"));
+	}
+
+	@Test
+	public void test_loadPropertiesMultiline() throws Exception
+	{
+		File file = new File("src/test/resources/test.properties");
+		Properties p = PropertiesUtil.loadProperties(file);
+		assertEquals("Hey,Whassup?", p.getProperty("multiline"));
+		assertEquals("Hey,\nEverything ok?", p.getProperty("multiline2"));
+	}
+
+	@Test
+	public void test_storeProperties() throws Exception
+	{
+		Properties p = new Properties();
+		p.setProperty("lala", "1");
+		p.setProperty("xxxx", "yyyy");
+		for(int i = 0; i < 100; i++)
+			p.setProperty("x." + i, i + "_x");
+
+		File normal = new File("normal.properties");
+		File zipped = new File("zipped.properties");
+
+		PropertiesUtil.storeProperties(normal, p, "some comments", false);
+		PropertiesUtil.storeProperties(zipped, p, "some comments", true);
+
+		Properties p1 = PropertiesUtil.loadProperties(normal, false);
+		Properties p2 = PropertiesUtil.loadProperties(zipped, true);
+
+		for(Object key : p.keySet())
+		{
+			assertEquals(p.get(key), p1.get(key));
+			assertEquals(p.get(key), p2.get(key));
+		}
 	}
 }
