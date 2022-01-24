3bfc102530c7f4065c148a6b9d9cbeea0623c5cc
ultimate <ultimatej@gmx.net>
2022-01-24 15:54:03 +0100

    added PlannedGame and Options

---
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/model/official/PlannedGame.java
@@ -0,0 +1,92 @@
+package ultimate.karoapi4j.model.official;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import ultimate.karoapi4j.model.base.Identifiable;
+
+public class PlannedGame extends Identifiable
+{
+	/*
+	 * from https://www.karopapier.de/api/example/game/new
+	 * "name": "Neues Spiel",
+	 * "map": 105,
+	 * "players": [ 2241 ],
+	 * "options": { .. } // see options
+	 */
+	private String	name;
+	private int		map;
+	private int[]	players;
+	private Options	options;
+
+	// additional properties
+	@JsonInclude(value = Include.NON_DEFAULT)
+	private boolean	created;
+	@JsonInclude(value = Include.NON_DEFAULT)
+	private boolean	left;
+
+	public PlannedGame()
+	{
+
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getMap()
+	{
+		return map;
+	}
+
+	public void setMap(int map)
+	{
+		this.map = map;
+	}
+
+	public int[] getPlayers()
+	{
+		return players;
+	}
+
+	public void setPlayers(int[] players)
+	{
+		this.players = players;
+	}
+
+	public Options getOptions()
+	{
+		return options;
+	}
+
+	public void setOptions(Options options)
+	{
+		this.options = options;
+	}
+
+	public boolean isCreated()
+	{
+		return created;
+	}
+
+	public void setCreated(boolean created)
+	{
+		this.created = created;
+	}
+
+	public boolean isLeft()
+	{
+		return left;
+	}
+
+	public void setLeft(boolean left)
+	{
+		this.left = left;
+	}
+}
