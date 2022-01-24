7e92e339f090de4a55c7e0e87eee746e3b45b9a0
ultimate <ultimatej@gmx.net>
2022-01-24 15:54:03 +0100

    added PlannedGame and Options

---
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/model/official/Options.java
@@ -0,0 +1,64 @@
+package ultimate.karoapi4j.model.official;
+
+import ultimate.karoapi4j.enums.EnumGameDirection;
+import ultimate.karoapi4j.enums.EnumGameTC;
+
+public class Options
+{
+	/*
+	 * From game creation
+	 * "zzz": 2,
+	 * "cps": true,
+	 * "startdirection": "classic",
+	 * "crashallowed": "forbidden"
+	 */
+	private int					zzz;
+	private boolean				cps;
+	private EnumGameDirection	startdirection;
+	private EnumGameTC			crashallowed;
+
+	public Options()
+	{
+
+	}
+
+	public int getZzz()
+	{
+		return zzz;
+	}
+
+	public void setZzz(int zzz)
+	{
+		this.zzz = zzz;
+	}
+
+	public boolean isCps()
+	{
+		return cps;
+	}
+
+	public void setCps(boolean cps)
+	{
+		this.cps = cps;
+	}
+
+	public EnumGameDirection getStartdirection()
+	{
+		return startdirection;
+	}
+
+	public void setStartdirection(EnumGameDirection startdirection)
+	{
+		this.startdirection = startdirection;
+	}
+
+	public EnumGameTC getCrashallowed()
+	{
+		return crashallowed;
+	}
+
+	public void setCrashallowed(EnumGameTC crashallowed)
+	{
+		this.crashallowed = crashallowed;
+	}
+}
