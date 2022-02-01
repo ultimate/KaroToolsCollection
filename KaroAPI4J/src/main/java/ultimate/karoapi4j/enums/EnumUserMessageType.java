048f131575b5b6cfc17c32dc2af10c4a8b711d21
ultimate <ultimatej@gmx.net>
2022-02-01 14:56:15 +0100

    javadoc

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumUserMessageType.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumUserMessageType.java
@@ -1,7 +1,21 @@
 package ultimate.karoapi4j.enums;
 
-public enum EnumMessageType
+import ultimate.karoapi4j.KaroAPI;
+
+/**
+ * UserMessage type as defined by the {@link KaroAPI}
+ * 
+ * @see <a href="https://www.karopapier.de/api/">https://www.karopapier.de/api/</a>
+ * @author ultimate
+ */
+public enum EnumUserMessageType
 {
+	/**
+	 * RX
+	 */
 	rx,
+	/**
+	 * TX
+	 */
 	tx
 }
--- KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumUserState.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumUserState.java
@@ -1,7 +1,21 @@
 package ultimate.karoapi4j.enums;
-//TODO javadoc
+
+import ultimate.karoapi4j.KaroAPI;
+
+/**
+ * User state as defined by the {@link KaroAPI}
+ * 
+ * @see <a href="https://www.karopapier.de/api/">https://www.karopapier.de/api/</a>
+ * @author ultimate
+ */
 public enum EnumUserState
 {
+	/**
+	 * active
+	 */
 	active,
+	/**
+	 * inactive
+	 */
 	inactive,
 }
