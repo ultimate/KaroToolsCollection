cba5d8ae42e59f9bebf217269170f08ae8b79829
ultimate <ultimatej@gmx.net>
2022-02-01 18:13:45 +0100

    javadoc

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/utils/ReflectionsUtil.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/utils/ReflectionsUtil.java
@@ -6,6 +6,11 @@ import java.lang.reflect.Method;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Util class for some reflections based operations (for example copying fields from one object to another).
+ * 
+ * @author ultimate
+ */
 public abstract class ReflectionsUtil
 {
 	private static final Logger logger = LoggerFactory.getLogger(ReflectionsUtil.class);
@@ -18,12 +23,21 @@ public abstract class ReflectionsUtil
 
 	}
 
-	public static <T> void copyFields(T from, T to, boolean includeNull)
+	/**
+	 * Copy all fields from one object to another.<br>
+	 * Can be used for example, if a target object shall be updated from an API
+	 * 
+	 * @param <T>
+	 * @param source - the source which contains the fields to copy
+	 * @param target - the target to copy the fields to
+	 * @param includeNull - whether also to copy null values (will overwrite existing values)
+	 */
+	public static <T> void copyFields(T source, T target, boolean includeNull)
 	{
-		if(from == null || to == null)
+		if(source == null || target == null)
 			throw new IllegalArgumentException("from and to must not be null!");
 		@SuppressWarnings("unchecked")
-		Class<T> cls = (Class<T>) from.getClass();
+		Class<T> cls = (Class<T>) source.getClass();
 
 		Method[] methods = cls.getMethods();
 
@@ -40,12 +54,12 @@ public abstract class ReflectionsUtil
 
 			try
 			{
-				Object value = m.invoke(from);
+				Object value = m.invoke(source);
 				if(value == null && !includeNull)
 					continue;
 				logger.debug("copying field: " + getterName + " -> " + setterName + " value = " + value);
 				Method setter = cls.getMethod(setterName, m.getReturnType());
-				setter.invoke(to, value);
+				setter.invoke(target, value);
 			}
 			catch(NoSuchMethodException | IllegalArgumentException | IllegalAccessException | InvocationTargetException e)
 			{
