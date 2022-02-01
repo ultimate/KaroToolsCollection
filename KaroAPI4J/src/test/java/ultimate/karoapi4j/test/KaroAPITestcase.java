f89e5bf5a07bb4c6627adb1b2c7fd810224238e2
ultimate <ultimatej@gmx.net>
2022-02-01 14:37:58 +0100

    javadoc & testing

--- KaroAPI4J/src/test/java/ultimate/karoapi4j/test/KaroAPITestcase.java
+++ KaroAPI4J/src/test/java/ultimate/karoapi4j/test/KaroAPITestcase.java
@@ -13,17 +13,33 @@ import org.slf4j.LoggerFactory;
 import ultimate.karoapi4j.KaroAPI;
 import ultimate.karoapi4j.utils.PropertiesUtil;
 
+/**
+ * Base class for all test cases that need a {@link KaroAPI} instance
+ * 
+ * @author ultimate
+ */
 @TestInstance(Lifecycle.PER_CLASS)
 public class KaroAPITestcase
 {
 	/**
 	 * Logger-Instance
 	 */
-	protected transient final Logger	logger		= LoggerFactory.getLogger(getClass());
+	protected transient final Logger	logger	= LoggerFactory.getLogger(getClass());
 
-	protected static Properties properties;
-	protected static KaroAPI karoAPI;
-	
+	/**
+	 * The {@link Properties} used to initiate the {@link KaroAPI}
+	 */
+	protected static Properties			properties;
+	/**
+	 * The {@link KaroAPI} instance
+	 */
+	protected static KaroAPI			karoAPI;
+
+	/**
+	 * Load the {@link Properties} and initiate the {@link KaroAPI} instance once per class
+	 * 
+	 * @throws IOException - if loading the {@link Properties} fails
+	 */
 	@BeforeAll
 	public void setUpOnce() throws IOException
 	{
