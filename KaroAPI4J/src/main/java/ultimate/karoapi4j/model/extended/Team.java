e7e5d88dff6cadd2e0a70cfa0c89a02f1c5ab88e
ultimate <ultimatej@gmx.net>
2022-02-01 16:09:02 +0100

    javadoc

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/model/extended/Team.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/model/extended/Team.java
@@ -7,45 +7,121 @@ import java.util.List;
 import ultimate.karoapi4j.model.official.Map;
 import ultimate.karoapi4j.model.official.User;
 
+/**
+ * Simple POJO that defines a team.<br>
+ * Each team has
+ * <ul>
+ * <li>a name</li>
+ * <li>a list of members</li>
+ * <li>a home map (optional)</li>
+ * </ul>
+ * 
+ * @author ultimate
+ */
 public class Team implements Serializable
 {
+	/**
+	 * Default serialVersionUID
+	 */
 	private static final long	serialVersionUID	= 1L;
 
+	/**
+	 * the team name
+	 */
 	private String				name;
-	private List<User>			players;
+	/**
+	 * the list of members
+	 * 
+	 * @see User
+	 */
+	private List<User>			members;
+	/**
+	 * the (optional) home {@link Map}
+	 */
 	private Map					homeMap;
 
-	public Team(String name, List<User> players)
+	/**
+	 * Create a new team (without home {@link Map})
+	 * 
+	 * @param name - the team name
+	 * @param members - the list of members
+	 * @see User
+	 */
+	public Team(String name, List<User> members)
 	{
-		this.name = name;
-		this.players = new LinkedList<User>(players);
+		this(name, members, null);
 	}
 
+	/**
+	 * Create a new team (with home {@link Map})
+	 * 
+	 * @param name - the team name
+	 * @param members - the list of members
+	 * @param homeMap - the home {@link Map}
+	 * @see User
+	 */
+	public Team(String name, List<User> members, Map homeMap)
+	{
+		this.name = name;
+		this.members = new LinkedList<User>(members);
+		this.homeMap = homeMap;
+	}
+
+	/**
+	 * Get the team name
+	 * 
+	 * @return the team name
+	 */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * Set the team name
+	 * 
+	 * @param name - the new team name
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 
-	public List<User> getPlayers()
+	/**
+	 * Get the list of members
+	 * 
+	 * @return the list of members
+	 */
+	public List<User> getMembers()
 	{
-		return players;
+		return members;
 	}
 
-	public void setPlayers(List<User> players)
+	/**
+	 * Set the list of members
+	 * 
+	 * @param name - the list of members
+	 */
+	public void setMembers(List<User> members)
 	{
-		this.players = players;
+		this.members = members;
 	}
 
+	/**
+	 * Get the home {@link Map}
+	 * 
+	 * @return the home {@link Map}
+	 */
 	public Map getHomeMap()
 	{
 		return homeMap;
 	}
 
+	/**
+	 * Set the home {@link Map}
+	 * 
+	 * @param name - the home {@link Map}
+	 */
 	public void setHomeMap(Map homeMap)
 	{
 		this.homeMap = homeMap;
