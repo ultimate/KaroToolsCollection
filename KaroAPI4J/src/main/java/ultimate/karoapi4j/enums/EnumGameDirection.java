77b26e07e698f94d46ed4b035f9970c95829a92e
ultimate <ultimatej@gmx.net>
2022-02-01 14:51:19 +0100

    javadoc, removed random

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumGameDirection.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumGameDirection.java
@@ -1,24 +1,53 @@
 package ultimate.karoapi4j.enums;
 
-//TODO javadoc
+import ultimate.karoapi4j.KaroAPI;
+
+/**
+ * Game startdirection as defined by the {@link KaroAPI}
+ * 
+ * @see <a href="https://www.karopapier.de/api/">https://www.karopapier.de/api/</a>
+ * @author ultimate
+ */
 public enum EnumGameDirection
 {
+	/**
+	 * Classic (move away from the finish line)
+	 */
 	classic,
-	free,
+	/**
+	 * F1 (start by passing the finish line)
+	 */
 	formula1,
-	random;
-	
+	/**
+	 * free choice
+	 */
+	free;
+
+	/**
+	 * Get the int value (used in the classic php pages)
+	 * 
+	 * @return the value
+	 */
 	public int getValue()
 	{
-		if(this.equals(classic))
-			return 1;
-		if(this.equals(formula1))
-			return 2;
-		if(this.equals(free))
-			return 0;
-		return -1;
+		switch(this)
+		{
+			case classic:
+				return 1;
+			case formula1:
+				return 2;
+			case free:
+			default:
+				return 0;
+		}
 	}
-	
+
+	/**
+	 * Get the enum from the int value (used in the classic php pages)
+	 * 
+	 * @param value - the int value
+	 * @return the enum
+	 */
 	public static EnumGameDirection getByValue(int value)
 	{
 		for(EnumGameDirection e : values())
