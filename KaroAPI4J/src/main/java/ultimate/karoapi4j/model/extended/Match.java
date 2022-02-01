a3d31ff4b7d1244d51fe3cad9be06a4086442891
ultimate <ultimatej@gmx.net>
2022-02-01 16:09:02 +0100

    javadoc

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/model/extended/Match.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/model/extended/Match.java
@@ -2,13 +2,33 @@ package ultimate.karoapi4j.model.extended;
 
 import java.io.Serializable;
 
+/**
+ * Simple POJO that represents a match between 2 {@link Team}s
+ * 
+ * @author ultimate
+ */
 public class Match implements Serializable
 {
+	/**
+	 * Default serialVersionUID
+	 */
 	private static final long	serialVersionUID	= 1L;
 
+	/**
+	 * Team 1
+	 */
 	private Team				team1;
+	/**
+	 * Team 2
+	 */
 	private Team				team2;
 
+	/**
+	 * Create a new match betwenn 2 {@link Team}s
+	 * 
+	 * @param team1 - Team 1
+	 * @param team2 - Team 2
+	 */
 	public Match(Team team1, Team team2)
 	{
 		super();
@@ -16,16 +36,27 @@ public class Match implements Serializable
 		this.team2 = team2;
 	}
 
+	/**
+	 * Get Team 1
+	 * @return Team 1
+	 */
 	public Team getTeam1()
 	{
 		return team1;
 	}
 
+	/**
+	 * Get Team 2
+	 * @return Team 2
+	 */
 	public Team getTeam2()
 	{
 		return team2;
 	}
-	
+
+	/**
+	 * Helper method that swaps both teams.
+	 */
 	public void swapTeams()
 	{
 		Team tmp = team1;
