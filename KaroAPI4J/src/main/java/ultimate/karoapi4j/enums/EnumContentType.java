5d404d5d7ddd8ca92d5cfd08be7bce200cd98f20
ultimate <ultimatej@gmx.net>
2022-02-01 14:39:32 +0100

    javadoc & testing

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumContentType.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/enums/EnumContentType.java
@@ -1,7 +1,20 @@
 package ultimate.karoapi4j.enums;
-//TODO javadoc
+
+import ultimate.karoapi4j.utils.URLLoader;
+
+/**
+ * This enum is used to set the type for passing content to an {@link URLLoader}
+ * 
+ * @author ultimate
+ */
 public enum EnumContentType
 {
-	json,
-	text
+	/**
+	 * handle content as text
+	 */
+	text,
+	/**
+	 * handle content as json
+	 */
+	json
 }
