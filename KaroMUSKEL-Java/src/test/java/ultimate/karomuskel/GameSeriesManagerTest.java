0fe74d1803dc5b27913519404b37a8297058fa3a
ultimate <ultimatej@gmx.net>
2022-02-08 18:05:34 +0100

    added loadV2 test

---
+++ KaroMUSKEL-Java/src/test/java/ultimate/karomuskel/GameSeriesManagerTest.java
@@ -0,0 +1,27 @@
+package ultimate.karomuskel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import muskel2.model.series.SimpleGameSeries;
+
+@SuppressWarnings("deprecation")
+public class GameSeriesManagerTest
+{
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Test
+	public void test_loadV2() throws ClassNotFoundException, ClassCastException, IOException
+	{
+		SimpleGameSeries gs_simple = (SimpleGameSeries) GameSeriesManager.loadV2(new File("target/test-classes/simple.muskel2"));
+		assertNotNull(gs_simple);
+		assertEquals("${spieler.ersteller}s Spieleserie - Spiel ${i} auf Karte ${karte.id}, ${regeln.x}", gs_simple.title);
+	}
+}
