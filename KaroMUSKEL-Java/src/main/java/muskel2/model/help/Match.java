b106e8e9e606ef573bc09b6f42dba3e3b6d2a92d
ultimate <ultimatej@gmx.net>
2022-02-09 12:12:46 +0100

    clean-up

--- KaroMUSKEL-Java/src/main/java/muskel2/model/help/Match.java
+++ KaroMUSKEL-Java/src/main/java/muskel2/model/help/Match.java
@@ -1,34 +1,11 @@
 package muskel2.model.help;
 
 import java.io.Serializable;
+
 @Deprecated
 public class Match implements Serializable
 {
 	private static final long	serialVersionUID	= 1L;
 
-	private Team[]	teams;
-
-	public Match(Team... teams)
-	{
-		super();
-		this.teams = teams;
-	}
-	
-	public Team[] getTeams()
-	{
-		return teams;
-	}
-
-	public Team getTeam(int index)
-	{
-		return teams[index];
-	}
-
-	public void rotateTeams()
-	{
-		Team tmp = teams[0];
-		for(int i = 1; i < teams.length; i++)
-			teams[i-1] = teams[i];
-		teams[teams.length-1] = tmp;
-	}
+	public Team[]				teams;
 }
--- KaroMUSKEL-Java/src/main/java/muskel2/model/help/Team.java
+++ KaroMUSKEL-Java/src/main/java/muskel2/model/help/Team.java
@@ -1,7 +1,6 @@
 package muskel2.model.help;
 
 import java.io.Serializable;
-import java.util.LinkedList;
 import java.util.List;
 
 import muskel2.model.Map;
@@ -11,48 +10,7 @@ public class Team implements Serializable
 {
 	private static final long	serialVersionUID	= 1L;
 
-	private String				name;
-	private List<Player>		players;
-	private Map					homeMap;
-
-	public Team(String name, List<Player> players)
-	{
-		this.name = name;
-		this.players = new LinkedList<Player>(players);
-	}
-
-	public String getName()
-	{
-		return name;
-	}
-
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-
-	public List<Player> getPlayers()
-	{
-		return players;
-	}
-
-	public void setPlayers(List<Player> players)
-	{
-		this.players = players;
-	}
-
-	public Map getHomeMap()
-	{
-		return homeMap;
-	}
-
-	public void setHomeMap(Map homeMap)
-	{
-		this.homeMap = homeMap;
-	}
-	
-	public String toString()
-	{
-		return name;
-	}
+	public String				name;
+	public List<Player>		players;
+	public Map					homeMap;
 }
