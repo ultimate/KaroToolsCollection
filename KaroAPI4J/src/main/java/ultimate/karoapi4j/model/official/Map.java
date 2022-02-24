04b3a1923324e6897902e6f39d1581c0e447149c
ultimate <ultimatej@gmx.net>
2022-02-24 10:21:45 +0100

    #105

--- KaroAPI4J/src/main/java/ultimate/karoapi4j/model/official/Map.java
+++ KaroAPI4J/src/main/java/ultimate/karoapi4j/model/official/Map.java
@@ -205,7 +205,6 @@ public class Map extends Identifiable
 	@Override
 	public String toString()
 	{
-		// #105
 		return "Karte " + this.getId() + ": " + this.name + " (" + this.players + " Spieler) von '" + author + "' " + (this.night ? "'Nacht'" : "'Tag'");
 	}
 }
