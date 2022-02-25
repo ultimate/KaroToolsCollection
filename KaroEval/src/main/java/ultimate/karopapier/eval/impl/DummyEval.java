6b6913acf5db72be83d45b646c05d43e485edb27
ultimate <ultimatej@gmx.net>
2022-02-25 16:07:51 +0100

    fixed

--- KaroEval/src/main/java/ultimate/karopapier/eval/impl/DummyEval.java
+++ KaroEval/src/main/java/ultimate/karopapier/eval/impl/DummyEval.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import muskel2.model.GameSeries;
+import ultimate.karoapi4j.model.extended.GameSeries;
 import ultimate.karopapier.eval.Eval;
 
 public class DummyEval implements Eval
@@ -15,7 +15,7 @@ public class DummyEval implements Eval
 	public String doEvaluation() throws Exception
 	{
 		String content = "Test fuer Automatische Auswertung " + Math.random() + " --~~~~";
-		writeFile(new File("dummy.txt"), content);		
+		writeFile(new File("dummy.txt"), content);
 		return content;
 	}
 
@@ -45,6 +45,5 @@ public class DummyEval implements Eval
 	public void prepare(GameSeries gs, int execution)
 	{
 		// TODO Auto-generated method stub
-		
 	}
 }
