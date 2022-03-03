fdcf695e0c405ed6d14ec76806f79353a92754f0
ultimate <ultimatej@gmx.net>
2022-03-03 17:01:18 +0100

    restore

--- KaroMUSKEL-Java/src/main/java/ultimate/karomuskel/ui/Screen.java
+++ KaroMUSKEL-Java/src/main/java/ultimate/karomuskel/ui/Screen.java
@@ -1,10 +1,6 @@
 package ultimate.karomuskel.ui;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.util.Arrays;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -109,93 +105,19 @@ public abstract class Screen extends JPanel
 	protected void setIcons(JRadioButton radioButton, Integer size)
 	{
 		radioButton.setIcon(createIcon("img/Radio-Normal.gif", size));
-		// radioButton.setPressedIcon(createIcon("img/Radio-Pressed.gif", size));
-		// radioButton.setRolloverIcon(createIcon("img/Radio-Normal-Hover.gif", size));
-		// radioButton.setRolloverSelectedIcon(createIcon("img/Radio-Active-Hover.gif", size));
-		// radioButton.setSelectedIcon(createIcon("img/Radio-Active.gif", size));
+		radioButton.setPressedIcon(createIcon("img/Radio-Pressed.gif", size));
+		radioButton.setRolloverIcon(createIcon("img/Radio-Normal-Hover.gif", size));
+		radioButton.setRolloverSelectedIcon(createIcon("img/Radio-Active-Hover.gif", size));
+		radioButton.setSelectedIcon(createIcon("img/Radio-Active.gif", size));
 	}
 
 	public static double rot = -Math.PI;
 
 	protected ImageIcon createIcon(String src, Integer size)
 	{
-		int drawSize = 100;
-		BufferedImage image = new BufferedImage(drawSize, drawSize, BufferedImage.TYPE_4BYTE_ABGR);
-
-		Color cBase = Color.red.darker();
-		Color c;
-
-		int rButton = drawSize * 4 / 5 / 2;
-		int rTop = drawSize / 4;
-		int cx = image.getWidth() / 2;
-		int cy = image.getHeight() / 2;
-		double rPixel;
-		double thetaPixel;
-		int r, g, b;
-		double rotation = rot;
-		rot += Math.PI/2;
-		double deltaRot;
-		
-		for(int x = 0; x < drawSize; x++)
-		{
-			for(int y = 0; y < drawSize; y++)
-			{
-				rPixel = Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
-				thetaPixel = Math.atan2(y - cy, x - cx);
-				deltaRot = (Math.abs(thetaPixel - rotation) / (Math.PI));
-				while(deltaRot > 1)
-					deltaRot -= 2;
-				while(deltaRot < -1)
-					deltaRot += 2;
-
-				// logger.info(x+ "|" + y + ", \tr=" + rPixel + ", \ttheta=" + thetaPixel + ",\t" + deltaRot);
-				if(rPixel < rButton)
-				{
-//					if(deltaRot > 0)
-//					{
-//						r = Math.min((int) (cBase.getRed() + deltaRot * (255 - cBase.getRed())), 255);
-//						g = Math.min((int) (cBase.getGreen() + deltaRot * (255 - cBase.getGreen())), 255);
-//						b = Math.min((int) (cBase.getBlue() + deltaRot * (255 - cBase.getBlue())), 255);
-//					}
-//					else
-//					{
-//						r = Math.max((int) (cBase.getRed() + deltaRot * (cBase.getRed())), 0);
-//						g = Math.max((int) (cBase.getGreen() + deltaRot * (cBase.getGreen())), 0);
-//						b = Math.max((int) (cBase.getBlue() + deltaRot * (cBase.getBlue())), 0);
-//					}
-					r = g = b = (int) (deltaRot * 100 + 100);
-					
-					c = new Color(r, g, b, cBase.getAlpha());
-					if(rPixel < rTop)
-						image.setRGB(x, y, cBase.getRGB());
-					else
-						image.setRGB(x, y, c.getRGB());
-				}
-			}
-		}
-
-		// int d = drawSize * 4 / 5;
-		// int gap = (drawSize - d) / 2;
-		// int shadowSize = gap;
-		// int shadowStep = 255 / shadowSize;
-		//
-		// logger.info("drawSize = " + drawSize);
-		// logger.info("d = " + d);
-		// logger.info("gap = " + gap);
-		// logger.info("shadowStep = " + shadowStep);
-		//
-		// g.setColor(new Color(0, 0, 0, shadowStep));
-		// for(int i = 0; i < shadowSize; i++)
-		// g.fillOval(gap + i, gap + i, d, d);
-		//
-		// g.setColor(Color.red.darker());
-		// g.fillOval(gap, gap, d, d);
-
-		return new ImageIcon(image.getScaledInstance(size, size, Image.SCALE_SMOOTH));
-
-		 ImageIcon imageIcon = new ImageIcon(src);
-		 if(size == null || imageIcon.getIconHeight() == size)
-		 return imageIcon;
-		 return new ImageIcon(imageIcon.getImage().getScaledInstance(size, size, Image.SCALE_SMOOTH));
+		ImageIcon imageIcon = new ImageIcon(src);
+		if(size == null || imageIcon.getIconHeight() == size)
+			return imageIcon;
+		return new ImageIcon(imageIcon.getImage().getScaledInstance(size, size, Image.SCALE_SMOOTH));
 	}
 }
