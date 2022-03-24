ba169bd4f259aed5b31ca51f51917b45645e5b2e
ultimate <ultimatej@gmx.net>
2022-03-24 15:26:16 +0100

    implemented #109

--- KaroEval/src/main/java/ultimate/karopapier/eval/Eval.java
+++ KaroEval/src/main/java/ultimate/karopapier/eval/Eval.java
@@ -1,7 +1,9 @@
 package ultimate.karopapier.eval;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,7 +19,6 @@ import ultimate.karoapi4j.KaroAPICache;
 import ultimate.karoapi4j.model.extended.GameSeries;
 import ultimate.karoapi4j.model.official.Game;
 import ultimate.karoapi4j.model.official.PlannedGame;
-import ultimate.karoapi4j.utils.PropertiesUtil;
 
 public abstract class Eval<T>
 {
@@ -45,12 +46,12 @@ public abstract class Eval<T>
 		this.execution = execution;
 	}
 
-	public abstract int doEvaluation() throws Exception; // TODO check meaningful return type
+	public abstract List<File> doEvaluation() throws Exception;
 
 	public void loadGameDetails(List<Game> gamesToLoad)
 	{
 		List<CompletableFuture<?>> cfs = new ArrayList<>(gamesToLoad.size());
-		for(Game game: gamesToLoad)
+		for(Game game : gamesToLoad)
 			cfs.add(this.karoAPICache.refresh(game));
 
 		CompletableFuture.allOf(cfs.toArray(new CompletableFuture[cfs.size()])).join();
@@ -69,20 +70,25 @@ public abstract class Eval<T>
 		}
 		loadGameDetails(gamesToLoad);
 	}
+	
 	// HELPERS
 
-	protected void writeProperties(String filename, Properties p) throws IOException
+	protected String readFile(String filename) throws IOException
 	{
-		PropertiesUtil.storeProperties(new File(folder, filename), p, null);
+		File file = new File(folder, filename);
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+		String content = new String(bis.readAllBytes());
+		bis.close();
+		return content;
 	}
 
-	protected void writeFile(String filename, String content) throws IOException
+	protected File writeFile(String filename, String content) throws IOException
 	{
-		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(folder, filename)));
-
+		File file = new File(folder, filename);
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
 		bos.write(content.getBytes());
-
 		bos.flush();
 		bos.close();
+		return file;
 	}
 }
