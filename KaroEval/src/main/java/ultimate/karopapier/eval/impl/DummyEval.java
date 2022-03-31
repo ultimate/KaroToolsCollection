3907a21e6b87874da292fe844cc580d7053e21ba
ultimate <ultimatej@gmx.net>
2022-03-31 13:42:09 +0200

    updated DummyEval #109

--- KaroEval/src/main/java/ultimate/karopapier/eval/impl/DummyEval.java
+++ KaroEval/src/main/java/ultimate/karopapier/eval/impl/DummyEval.java
@@ -1,49 +1,18 @@
 package ultimate.karopapier.eval.impl;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ultimate.karoapi4j.model.extended.GameSeries;
 import ultimate.karopapier.eval.Eval;
 
-public class DummyEval implements Eval
+public class DummyEval extends Eval<GameSeries>
 {
-
 	@Override
-	public String evaluate() throws Exception
+	public List<File> evaluate()
 	{
-		String content = "Test fuer Automatische Auswertung " + Math.random() + " --~~~~";
-		writeFile(new File("dummy.txt"), content);
-		return content;
-	}
-
-	public static void writeFile(File target, String content) throws IOException
-	{
-		if(!target.getAbsoluteFile().getParentFile().exists())
-			target.getAbsoluteFile().getParentFile().mkdirs();
-		BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(target));
-		try
-		{
-			byte[] bytes = content.getBytes();
-			fos.write(bytes);
-			fos.flush();
-		}
-		catch(IOException e)
-		{
-			throw e;
-		}
-		finally
-		{
-			if(fos != null)
-				fos.close();
-		}
-	}
-
-	@Override
-	public void prepare(GameSeries gs, int execution)
-	{
-		// do nothing
+		logger.info("dummy eval...");
+		return new ArrayList<>();
 	}
 }
