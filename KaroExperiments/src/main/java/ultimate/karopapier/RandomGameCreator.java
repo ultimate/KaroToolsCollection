f45753cdd03856b9a5aeb809eb3088c15858768b
JVerkin <JVerkin@NB-MED-CH-0145.hamilton.ch>
2022-11-09 18:08:24 +0100

    rumspielen

--- KaroExperiments/src/main/java/ultimate/karopapier/RandomGameCreator.java
+++ KaroExperiments/src/main/java/ultimate/karopapier/RandomGameCreator.java
@@ -2,8 +2,10 @@ package ultimate.karopapier;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.Properties;
 import java.util.Random;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +16,7 @@ import ultimate.karoapi4j.enums.EnumGameDirection;
 import ultimate.karoapi4j.enums.EnumGameTC;
 import ultimate.karoapi4j.model.extended.Rules;
 import ultimate.karoapi4j.model.official.PlannedGame;
+import ultimate.karoapi4j.model.official.User;
 import ultimate.karoapi4j.utils.PropertiesUtil;
 
 public class RandomGameCreator
@@ -33,6 +36,10 @@ public class RandomGameCreator
 
 		KaroAPI api = new KaroAPI(login.getProperty("karoapi.user"), login.getProperty("karoapi.password"));
 		KaroAPICache cache = new KaroAPICache(api);
+		cache.refresh().join();
+		
+		logger.debug("users: " + cache.getUsers().size());
+		logger.debug("maps:  " + cache.getMaps().size());
 
 		int gamesCount = Integer.parseInt(gameseries.getProperty("games.count"));
 		int gamesPerExecution = Integer.parseInt(gameseries.getProperty("games.perExecution"));
@@ -47,16 +54,50 @@ public class RandomGameCreator
 		rules.setMinZzz(Integer.parseInt(gameseries.getProperty("rules.minZzz")));
 		rules.setStartdirection(EnumGameDirection.valueOf(gameseries.getProperty("rules.startdirection")));
 		logger.info(rules);
+		
+		logger.info("creating player list...");
+		String[] playerIDs = gameseries.getProperty("players").split(",");
+		Set<User> players = new LinkedHashSet<>();
+		int id;
+		User user;
+		StringBuilder sb = new StringBuilder();
+		for(String player: playerIDs)
+		{
+			try
+			{
+				id = Integer.parseInt(player);
+				user = cache.getUser(id);
+			}
+			catch(NumberFormatException e)
+			{
+				user = cache.getUser(player);
+			}
+			
+			players.add(user);
+			sb.append("\n -> ");
+			sb.append(user);
+		}
+		logger.info("Players:" + sb);
 
+		logger.info("creating games...");
 		Random random = new Random();
 		PlannedGame pg;
-		for(int i = 1; i < gamesPerExecution; i++)
+		int mapID;
+		for(int i = gamesCount + 1; i <= gamesCount + gamesPerExecution; i++)
 		{
+			if(gameseries.getProperty("map").equalsIgnoreCase("%random"))
+				mapID = random.nextInt(cache.getMaps().size());
+			else
+				mapID = Integer.parseInt(gameseries.getProperty("map"));
+
 			pg = new PlannedGame();
-			pg.setName(gameseries.getProperty("name").replace("%i", "" + (gamesCount + i)));
-			pg.setMap( todo );
-			pg.setPlayers( todo );
+			pg.setName(gameseries.getProperty("name").replace("%i", "" + i));
+			pg.setMap(cache.getMap(mapID));
+			pg.setPlayers(players);
 			pg.setOptions(rules.createOptions(random));
+			
+			logger.info("Game #" + i);
+			
 		}
 
 		gameseries.setProperty("games.count", "" + (gamesCount + gamesPerExecution));
