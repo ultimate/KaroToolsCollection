42e0803a237d718f8e83d43a42c284ee971fe1a7
ultimate <ultimatej@gmx.net>
2024-02-20 18:00:41 +0100

    fix test #218

--- KaroRAUPE/src/test/java/ultimate/karoraupe/rules/NoPossiblesRuleTest.java
+++ KaroRAUPE/src/test/java/ultimate/karoraupe/rules/NoPossiblesRuleTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import ultimate.karoapi4j.model.official.Game;
 import ultimate.karoapi4j.model.official.Move;
 import ultimate.karoapi4j.model.official.Player;
 import ultimate.karoraupe.rules.Rule.Result;
@@ -40,10 +41,12 @@ public class NoPossiblesRuleTest extends KaroRAUPETestcase
 	@MethodSource("providePossibles")
 	public void test_evaluate(List<Move> possibles, Boolean expected)
 	{
+		Game game = new Game(123);
+		
 		Player player = new Player();
 		player.setPossibles(possibles);
 
-		Result result = rule.evaluate(null, player, null);
+		Result result = rule.evaluate(game, player, null);
 		
 		assertNotNull(result);
 		assertEquals(expected, result.shallMove());
